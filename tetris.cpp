73f64ac2ce10fe8f2f8df85f1df2e3bf8cda45bf
wortelworm <wortelworm@gmail.com>
2023-02-19 08:34:27 +0100

    -

--- tetris.cpp
+++ tetris.cpp
@@ -13,25 +13,11 @@
 #define tetrisPixel(x, y, color) Utils::DrawPixel(x + 2, y + 2, color);
 #define tetrisBlock(x, y, color) Utils::FillRect((x)*3 + 2, (y)*3 + 2, 3, 3, color);
 
-const Tetris::Piece Pieces[] = {
-  // I piece (sideways)
-  { CYAN,    15,  0, Tetris::RotationType::special},
-  // J piece
-  { BLUE,    142, 0, Tetris::RotationType::normal },
-  // L piece
-  { ORANGE,  46,  0, Tetris::RotationType::normal },
-  // O piece
-  { YELLOW,  102, 0, Tetris::RotationType::none   },
-  // S piece
-  { GREEN,   108, 0, Tetris::RotationType::normal },
-  // Z piece
-  { RED,     198, 0, Tetris::RotationType::normal },
-  // T piece
-  { MAGENTA, 78,  0, Tetris::RotationType::normal },
-};
+
 
 
 namespace Tetris {
+
   uint8_t field[25];
   uint8_t bag[7];
   uint8_t bagPointer;
@@ -41,12 +27,11 @@ namespace Tetris {
   Piece previewPiece;
 
   void Play() {
-    // TODO
     Utils::FillRect(0, 0, 64, 64, BLACK);
 
     // draw outlines of boxes
     for (int8_t i = 0; i < 30; i++) {
-      tetrisPixel(i, -1,  WHITE);
+      tetrisPixel(i, -1, WHITE);
       tetrisPixel(i, 60, WHITE);
     }
     for (int8_t j = 0; j < 62; j++) {
@@ -55,76 +40,22 @@ namespace Tetris {
     }
 
     bagPointer = 7;
-    fallingPiece = { 0, 0, 0, Tetris::RotationType::normal };
+    fallingPiece = { 0, 0, 0, RotationType::normal };
     memset(field, 0, 25);
     createPiece();
 
     while (! digitalRead(BUTTON_MENU)) {
       createPiece();
+      delay(500);
       while (! moveFallingPiece()) {
-        delay(500);
-      }
-      delay(1000);
-    }
-  }
-
-  void displayPiece(Piece piece) {
-    // TODO?
-  }
-
-  bool moveFallingPiece() {
-    // check for collision
-    fallingY++;
-    for (int8_t i = 0; i < 8; i++) {
-      uint8_t upperpos = (fallingX + (i%4)) + 10 * (fallingY + (i/4));
-      uint8_t lowerpos = upperpos + 20;
-
-      if (fallingPiece.upperShape&(1<<(7-i)) != 0) {
-        // block of the piece here
-        if (fallingY+1 + (i/4) >= 20) {
-          // collision with bottom of the screen
-          return true;
-        }
-        if (field[upperpos%8]&(upperpos/8) != 0) {
-          // collision with something on the field
-          return true;
+        for (int8_t i = 0; i < 30; i++) {
+          readInputs();
+          delay(10);
         }
       }
-
-      if (fallingPiece.lowerShape&(1<<(7-i)) != 0) {
-        // block of the piece here
-        if (fallingY+1 + (i/4) + 2 >= 20) {
-          // collision with bottom of the screen
-          return true;
-        }
-        if (field[lowerpos%8]&(lowerpos/8) != 0) {
-          // collision with something on the field
-          return true;
-        }
-      }
+      endOfFalling();
+      delay(500);
     }
-    
-    // remove the current drawing
-    for (int8_t i = 0; i < 8; i++) {
-      if ((fallingPiece.upperShape & (1 << (7 - i))) != 0) {
-        tetrisBlock(fallingX+(i%4), fallingY-1 +(i>>2),       BLACK);
-      }
-      if ((fallingPiece.lowerShape & (1 << (7 - i))) != 0) {
-        tetrisBlock(fallingX+(i%4), fallingY-1 +(i>>2) + 2, BLACK);
-      }
-    }
-
-    // add updated drawing
-    for (int8_t i = 0; i < 8; i++) {
-      if ((fallingPiece.upperShape&(1<<(7-i))) != 0) {
-        tetrisBlock(fallingX+(i%4), fallingY+(i>>2),     fallingPiece.color);
-      }
-      if ((fallingPiece.lowerShape&(1<<(7-i))) != 0) {
-        tetrisBlock(fallingX+(i%4), fallingY+(i>>2)+2, fallingPiece.color);
-      }
-    }
-
-    return false;
   }
 
   void createPiece() {
@@ -154,14 +85,113 @@ namespace Tetris {
 
     // show the preview piece
     for (int8_t i = 0; i < 8; i++) {
-      tetrisBlock(12 + (i % 4), (i >> 2),      ((previewPiece.upperShape & (1 << (7 - i))) == 0) ? BLACK : previewPiece.color);
+      tetrisBlock(12 + (i % 4), (i >> 2),     ((previewPiece.upperShape & (1 << (7 - i))) == 0) ? BLACK : previewPiece.color);
       tetrisBlock(12 + (i % 4), (i >> 2) + 2, ((previewPiece.lowerShape & (1 << (7 - i))) == 0) ? BLACK : previewPiece.color);
     }
 
     // show piece
     for (int8_t i = 0; i < 8; i++) {
-      tetrisBlock(fallingX + (i % 4), fallingY + (i >> 2),      ((fallingPiece.upperShape & (1 << (7 - i))) == 0) ? BLACK : fallingPiece.color);
-      tetrisBlock(fallingX + (i % 4), fallingY + (i >> 2) + 2, ((fallingPiece.lowerShape & (1 << (7 - i))) == 0) ? BLACK : fallingPiece.color);
+      if ((fallingPiece.upperShape & (1 << (7 - i))) != 0) {
+        tetrisBlock(fallingX+(i%4), fallingY+(i>>2),   fallingPiece.color);
+      }
+      if ((fallingPiece.lowerShape & (1 << (7 - i))) != 0) {
+        tetrisBlock(fallingX+(i%4), fallingY+(i>>2)+2, fallingPiece.color);
+      }
     }
+  }
+
+  bool moveFallingPiece() {
+    // check for collision
+    fallingY++;
+    for (int8_t i = 0; i < 8; i++) {
+      uint8_t upperpos = (fallingX + (i%4)) + 10 * (fallingY + (i/4));
+      uint8_t lowerpos = upperpos + 10;
+
+      if ((fallingPiece.upperShape&(1<<(7-i))) != 0) {
+        // block of the piece here
+        if (fallingY + (i/4) >= 20) {
+          // collision with bottom of the screen
+          fallingY--;
+          return true;
+        }
+        if ((field[upperpos/8]&(1<<(upperpos%8))) != 0) {
+          // collision with something on the field
+          fallingY--;
+          return true;
+        }
+      }
+
+      if ((fallingPiece.lowerShape&(1<<(7-i))) != 0) {
+        // block of the piece here
+        if (fallingY + (i/4) + 2 >= 20) {
+          // collision with bottom of the screen
+          fallingY--;
+          return true;
+        }
+        if ((field[lowerpos/8]&(1<<(lowerpos%8))) != 0) {
+          // collision with something on the field
+          fallingY--;
+          return true;
+        }
+      }
+    }
+    
+    // remove the current drawing
+    for (int8_t i = 0; i < 8; i++) {
+      if ((fallingPiece.upperShape & (1 << (7 - i))) != 0) {
+        tetrisBlock(fallingX+(i%4), fallingY-1 +(i>>2),     BLACK);
+      }
+      if ((fallingPiece.lowerShape & (1 << (7 - i))) != 0) {
+        tetrisBlock(fallingX+(i%4), fallingY-1 +(i>>2) + 2, BLACK);
+      }
+    }
+
+    // add updated drawing
+    for (int8_t i = 0; i < 8; i++) {
+      if ((fallingPiece.upperShape&(1<<(7-i))) != 0) {
+        tetrisBlock(fallingX+(i%4), fallingY+(i>>2),   fallingPiece.color);
+      }
+      if ((fallingPiece.lowerShape&(1<<(7-i))) != 0) {
+        tetrisBlock(fallingX+(i%4), fallingY+(i>>2)+2, fallingPiece.color);
+      }
+    }
+
+    return false;
+  }
+
+  void readInputs() {
+    // TODO collission detection
+    // TODO button remembering
+    // TODO redraw
+    if (digitalRead(BUTTON_LEFT)) {
+      if (fallingX > 0) {
+        fallingX--;
+      }
+    }
+
+    if (digitalRead(BUTTON_RIGHT)) {
+      if (fallingX < 6) {
+        fallingX ++;
+      }
+    }
+  }
+
+  void endOfFalling() {
+    // solidifies the currently falling piece
+    for (int8_t i = 0; i < 8; i++) {
+      uint8_t upperpos = (fallingX + (i%4)) + 10 * (fallingY + (i/4));
+      uint8_t lowerpos = upperpos + 10;
+
+      if ((fallingPiece.upperShape&(1<<(7-i))) != 0) {
+        field[upperpos/8] |= 1 << (upperpos%8);
+      }
+      if ((fallingPiece.lowerShape&(1<<(7-i))) != 0) {
+        field[lowerpos/8] |= 1 << (lowerpos%8);
+      }
+    }
+
+    fallingPiece = { 0, 0, 0, RotationType::none };
+
+    // TODO: check for completed row
   }
 }
--- tetris.h
+++ tetris.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "utils.h"
 
 namespace Tetris {
   void Play();
@@ -11,6 +12,8 @@ namespace Tetris {
    * @returns true if the piece stops with falling, false otherwise
    */
   bool moveFallingPiece();
+  void endOfFalling();
+  void readInputs();
 
   enum RotationType {
     normal, special, none
@@ -20,5 +23,21 @@ namespace Tetris {
     uint8_t upperShape;
     uint8_t lowerShape;
     RotationType rotation;
+  };
+  const Piece Pieces[] = {
+    // I piece (sideways)
+    { CYAN,    15,  0, RotationType::special},
+    // J piece
+    { BLUE,    142, 0, RotationType::normal },
+    // L piece
+    { ORANGE,  46,  0, RotationType::normal },
+    // O piece
+    { YELLOW,  102, 0, RotationType::none   },
+    // S piece
+    { GREEN,   108, 0, RotationType::normal },
+    // Z piece
+    { RED,     198, 0, RotationType::normal },
+    // T piece
+    { MAGENTA, 78,  0, RotationType::normal },
   };
 }
